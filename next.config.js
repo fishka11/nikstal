ef69876468633aba2b1f7972811f5e5671a46974
Rafal Piasnik <r.piasnik@gmail.com>
2023-06-15 22:12:28 +0200

    some redirections

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ["media.graphassets.com"] },
+  async redirects() {
+    return [
+      {
+        source: "/o-nas",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- src/app/components/Brand.js
+++ src/app/components/Brand.js
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 export default function Brand() {
   const pathname = usePathname();
   return (
-    <Link className="flex items-start" href="/">
+    <Link className="flex items-start" href="o-nas">
       <Image
         quality={100}
         alt="logo NikStal - skup złomu stalowego i metali kolorowych"
