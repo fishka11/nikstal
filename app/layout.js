12b411527e4cf152ed3cf50cc3fb29568366bf81
Rafal Piasnik <r.piasnik@gmail.com>
2023-09-24 11:50:31 +0200

    add cookies message

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,20 @@
+import Cookies from './components/cookies';
 import getData from './lib/fetchAPI';
 import { getLayoutsSEO } from './lib/queries';
-import Header from "./components/header";
-import Footer from "./components/footer";
-import { library, config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import Header from './components/header';
+import Footer from './components/footer';
+import { library, config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
 const inter = Inter({
-  subsets: ["latin", "latin-ext"],
+  subsets: ['latin', 'latin-ext'],
   preload: true,
-  display: "block",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  style: ["normal"],
+  display: 'block',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal'],
 });
 
 config.autoAddCss = false;
@@ -22,29 +23,29 @@ library.add(fas);
 export async function generateMetadata() {
   const data = await getData(getLayoutsSEO);
   const metaData = data.layoutsSEO.filter(
-    (layout) => layout.name === "rootLayoutSEO"
+    layout => layout.name === 'rootLayoutSEO'
   );
 
   return {
     title: {
       default:
         metaData?.seo?.title ||
-        "Skup złomu Sosnowiec, skup złomu Jaworzno, Katowice, Śląsk, skup metali kolorowych",
+        'Skup złomu Sosnowiec, skup złomu Jaworzno, Katowice, Śląsk, skup metali kolorowych',
     },
     description:
       metaData?.seo?.description ||
-      "Skup złomu Sosnowiec - Nikstal - skupujemy metale kolorowe i surowce wtórne z rejonu Śląska i Zagłębia. Obsługujemy: Mysłowice, Sosnowiec, Jaworzno, Mikołów, Tychy, Zawiercie, Dąbrowa Górnicza, Katowice,  Skupujemy złom po najwyższych cenach w regionie! Prowadzimy skup hurtowy i detaliczny.",
+      'Skup złomu Sosnowiec - Nikstal - skupujemy metale kolorowe i surowce wtórne z rejonu Śląska i Zagłębia. Obsługujemy: Mysłowice, Sosnowiec, Jaworzno, Mikołów, Tychy, Zawiercie, Dąbrowa Górnicza, Katowice,  Skupujemy złom po najwyższych cenach w regionie! Prowadzimy skup hurtowy i detaliczny.',
     keywords:
       metaData?.seo?.keywords ||
-      "Skup złomu, Mysłowice, Sosnowiec, Jaworzno, Katowice, Tychy, Śląsk, Mikołów, Zawiercie, ceny skupu stali, miedzi, mosiądzu, aluminium, cynku, ołowiu, makulatury, akumulatorów, hurtowy, detaliczny",
-    authors: [{ name: "Rafał Piaśnik", url: "" }],
+      'Skup złomu, Mysłowice, Sosnowiec, Jaworzno, Katowice, Tychy, Śląsk, Mikołów, Zawiercie, ceny skupu stali, miedzi, mosiądzu, aluminium, cynku, ołowiu, makulatury, akumulatorów, hurtowy, detaliczny',
+    authors: [{ name: 'Rafał Piaśnik', url: '' }],
     formatDetection: {
       email: true,
       address: false,
       telephone: true,
     },
     verification: {
-      google: "i5ZygHCqAwgik27FxY6hJ1MOfBvANHBBprYJB4JI8oo",
+      google: 'i5ZygHCqAwgik27FxY6hJ1MOfBvANHBBprYJB4JI8oo',
     },
   };
 }
@@ -56,6 +57,7 @@ export default function RootLayout({ children, params }) {
         <Header />
         <main className="relative">{children}</main>
         <Footer />
+        <Cookies />
       </body>
     </html>
   );
